9f2fcc0f4b3c4bf9447aadf2d5101afd81e5b317
wkcho99 <wkcho@postech.ac.kr>
2022-02-08 16:46:55 +0900

    calender temp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "react-datepicker/dist/react-datepicker.css"; // css import 
-import ButtonAction from "./components/buttonAction/buttonAction";
+
 import "./App.css"
 import React, { useEffect, useState } from "react";
-import Header from "./components/header/header";
+
 import Main from "./components/main/main";
 let date = new Date();
 function App() {
@@ -11,9 +11,9 @@ function App() {
     const [wtfsdates,setWtfsDates] = useState([]);
   return (
     <div className="App">
-      {/* <Header month = {month} setMonth = {setMonth}/> */}
+
       <Main viewMonth = {viewMonth} setViewMonth = {setViewMonth} smtdates = {smtdates} setSmtDates = {setSmtDates} wtfsdates = {wtfsdates} setWtfsDates = {setWtfsDates}/>
-      {/* <ButtonAction month = {month} setMonth = {setMonth}/> */}
+
     </div>
   );
 }
